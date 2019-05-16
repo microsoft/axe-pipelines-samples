89b3a996ef1c889c0e15a4fb4c496e88177f5334
Dan Bjorge <danielbj@microsoft.com>
2019-05-16 15:59:31 -0700

    add initial impl, in-progress on sarif

--- typescript-selenium-webdriver/tests/index.test.ts
+++ typescript-selenium-webdriver/tests/index.test.ts
@@ -2,6 +2,9 @@
 // Licensed under the MIT License.
 import { Builder, By, until, ThenableWebDriver } from 'selenium-webdriver';
 import * as path from 'path';
+import * as fs from 'fs';
+import * as AxeBuilder from 'axe-webdriverjs';
+import { exec } from 'child_process';
 
 describe('index.html', () => {
     const indexUri = 'file://' + path.join(__dirname, '..', 'src', 'index.html');
@@ -25,4 +28,33 @@ describe('index.html', () => {
         const headerText = await header.getText();
         expect(headerText).toEqual('This is a static sample page with some accessibility issues');
     });
+
+    it('passes accessibility checks', async () => {
+        const header = await driver.wait(until.elementLocated(By.css('h1')));
+        await driver.wait(until.elementIsVisible(header));
+
+        const axeResults = await AxeBuilder(driver)
+            .options({
+                runOnly: {
+                    type: "tag",
+                    values: ["wcag2a", "wcag2aa"]
+                }
+            })
+            .analyze();
+
+        /*
+        const sarifResults = convertAxeToSarif(axeResults);
+        await fs.promises.writeFile('./test-results/index.html.sarif', JSON.stringify(sarifResults))
+        await exec('./dotnet_tools/sarif match-results-object ./tests/index.test.ts.baseline ./test-results/index.html.sarif');
+        */
+
+        expect(axeResults).toMatchInlineSnapshot();
+    });
+
+    function getViolationFingerprints(axeResults) {
+        return axeResults.violations.map(v => { return {
+            id: v.id,
+            targets: v.nodes.map(n => n.target),
+        }});
+    }
 });
