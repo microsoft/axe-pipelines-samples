d16083457ad35b8afc314a92d77885219840d44c
Dan Bjorge <danielbj@microsoft.com>
2019-05-16 17:31:32 -0700

    Attempt using chrome driver to see if that works with windows-2019 agent

--- typescript-selenium-webdriver/tests/index.test.ts
+++ typescript-selenium-webdriver/tests/index.test.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { Builder, By, until, ThenableWebDriver } from 'selenium-webdriver';
+import { Options } from 'selenium-webdriver/chrome';
 import * as path from 'path';
 
 describe('index.html', () => {
@@ -8,7 +9,12 @@ describe('index.html', () => {
     let driver: ThenableWebDriver;
 
     beforeAll(async () => {
-        driver = new Builder().forBrowser('firefox').build();
+        jest.setTimeout(30000);
+
+        driver = new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(new Options().addArguments('--headless'))
+            .build();
     });
 
     afterAll(async () => {
@@ -17,7 +23,7 @@ describe('index.html', () => {
 
     beforeEach(async () => {
         await driver.get(indexUri);
-    })
+    });
 
     it('renders the expected header text', async () => {
         const header = await driver.wait(until.elementLocated(By.css('h1')));
