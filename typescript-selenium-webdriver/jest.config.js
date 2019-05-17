710b37d9afd63ab4563b62180212feefcacf9dba
Dan Bjorge <danielbj@microsoft.com>
2019-05-16 18:42:37 -0700

    Remove code coverage from sample (there is no non-test typescript to cover)

--- typescript-selenium-webdriver/jest.config.js
+++ typescript-selenium-webdriver/jest.config.js
@@ -3,10 +3,9 @@
 module.exports = {
     preset:'ts-jest',
     testEnvironment: 'node',
-    collectCoverage: true,
-    collectCoverageFrom: ['./**/*.ts', '!./**/*.test.ts'],
-    coverageDirectory: './test-results/coverage',
-    coverageReporters: ['json', 'lcov', 'text', 'cobertura'],
+
+    // Outputting in JUnit format is for the benefit of Azure Pipelines' test results view.
+    // We publish the JUnit results for it to see from the "publish test results" step in azure-pipelines.yml.
     reporters: [
         'default',
         [
