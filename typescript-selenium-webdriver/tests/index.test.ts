75a911ac37837d9d249fe3de112708f4378c5aed
Dan Bjorge <danielbj@microsoft.com>
2019-05-21 21:16:20 -0700

    Add sarif export back in to all new examples

--- typescript-selenium-webdriver/tests/index.test.ts
+++ typescript-selenium-webdriver/tests/index.test.ts
@@ -7,6 +7,9 @@ import * as path from 'path';
 import { Builder, By, ThenableWebDriver, until } from 'selenium-webdriver';
 import * as chrome from 'selenium-webdriver/chrome';
 import { promisify } from 'util';
+import * as Axe from 'axe-core';
+
+const TEST_TIMEOUT_MS = 30000;
 
 describe('index.html', () => {
     let driver: ThenableWebDriver;
@@ -47,37 +50,78 @@ describe('index.html', () => {
         // See https://jestjs.io/docs/en/testing-frameworks for examples.
         const pageUnderTest = 'file://' + path.join(__dirname, '..', 'src', 'index.html');
         await driver.get(pageUnderTest);
-    });
 
-    it('renders the expected header text', async () => {
-        const header = await driver.wait(until.elementLocated(By.css('h1')));
-        await driver.wait(until.elementIsVisible(header));
-        const headerText = await header.getText();
-
-        expect(headerText).toEqual('This is a static sample page with some accessibility issues');
-    });
-
-    it('only contains known accessibility violations', async () => {
         // Ensure that the page is loaded and rendered
-        const header = await driver.wait(until.elementLocated(By.css('h1')));
-        await driver.wait(until.elementIsVisible(header));
+        await driver.wait(until.elementLocated(By.css('main')));
+    }, 15000);
 
-        // Run an accessibility scan using axe-webdriverjs
-        const axeResults = await AxeBuilder(driver).analyze();
+    // This test case shows the most basic example: run a scan, fail the test if there are any failures.
+    // This is the way to go if you have no known/pre-existing violations you need to temporarily baseline.
+    it('has no accessibility violations in the h1 element', async () => {
+        const accessibilityScanResults = await AxeBuilder(driver)
+            .include('h1')
+            .analyze();
 
-        // Write a test expectation that accounts for "known" issues we want to baseline
-        expect(axeResults.violations.length).toBe(5);
+        await exportAxeAsSarifTestResult('index-h1-element.sarif', accessibilityScanResults);
 
-        // Write the axe results to a .sarif file, so we can use the SARIF Multitool to
-        // apply a baseline file and show the results in the Scans tab in Azure Pipelines
+        expect(accessibilityScanResults.violations).toStrictEqual([]);
+    }, TEST_TIMEOUT_MS);
+
+    // If you want to run a scan of a page but need to exclude an element with known issues (eg, a third-party
+    // component you don't control fixing yourself), you can exclude it specifically and still scan the rest
+    // of the page.
+    it('has no accessibility violations outside of the known example violations', async () => {
+        const accessibilityScanResults = await AxeBuilder(driver)
+            .exclude('#example-accessibility-violations')
+            .analyze();
+
+        await exportAxeAsSarifTestResult('index-except-examples.sarif', accessibilityScanResults);
+
+        expect(accessibilityScanResults.violations).toStrictEqual([]);
+    }, TEST_TIMEOUT_MS);
+
+    // If you want to more precisely baseline a particular set of known issues, one option is to use Jest
+    // Snapshot Testing (https://jestjs.io/docs/snapshot-testing) with the scan results object.
+    it('has only those accessibility violations present in snapshot', async () => {
+        const accessibilityScanResults = await AxeBuilder(driver).analyze();
+
+        await exportAxeAsSarifTestResult('index-page.sarif', accessibilityScanResults);
+
+        // Snapshotting the entire violations array like this will show the full available information in
+        // your test output for any new violations that might occur.
+        expect(accessibilityScanResults.violations).toMatchSnapshot();
+
+        // However, since the "full available information" includes contextual information like "a snippet
+        // of the HTML containing the violation" and "the full xpath to the element containing the violation",
+        // snapshotting the whole violations array is prone to failing when unrelated changes are made to the
+        // element (or even to unrelated ancestors of the element in the DOM).
+
+        // To avoid that, you can create a helper function to capture a "fingerprint" of a given violation,
+        // and snapshot that instead. The Jest Snapshot log output will only include the information from your
+        // fingerprint, but you can still use the exported .sarif files to see complete failure information
+        // in a SARIF viewer (https://sarifweb.azurewebsites.net/#Viewers) or a text editor.
+        const getViolationFingerprint = (violation: Axe.Result) => {
+            rule: violation.id;
+            targets: violation.nodes.map(node => node.target);
+        };
+        expect(accessibilityScanResults.violations.map(getViolationFingerprint)).toMatchSnapshot();
+    }, TEST_TIMEOUT_MS);
+
+    // Exporting axe results as .sarif files lets our Azure Pipelines build results page show a nice visualization
+    // of any accessibility failures we find using the Sarif Results Viewer Tab extension
+    // (https://marketplace.visualstudio.com/items?itemName=sariftools.sarif-viewer-build-tab)
+    async function exportAxeAsSarifTestResult(sarifFileName: string, axeResults: Axe.AxeResults): Promise<void> {
         const sarifResults = convertAxeToSarif(axeResults);
+
+        // This directory should be .gitignore'd and should be published as a build artifact in azure-pipelines.yml
         const testResultsDirectory = path.join(__dirname, '..', 'test-results');
         await promisify(fs.mkdir)(testResultsDirectory, { recursive: true });
+
+        const sarifResultFile = path.join(testResultsDirectory, sarifFileName);
         await promisify(fs.writeFile)(
-            path.join(testResultsDirectory, 'index.html.axe-core.sarif'),
-            // We'll be checking in the resulting .sarif file for baselining purposes, so
-            // it's a good idea to use a spacing argument (here, "2") to pretty-print the
-            // JSON. This makes it much more pleasant to diff when it changes. 
+            sarifResultFile,
             JSON.stringify(sarifResults, null, 2));
-    });
+
+        console.log(`Exported axe results to ${sarifResultFile}`);
+    }
 });
