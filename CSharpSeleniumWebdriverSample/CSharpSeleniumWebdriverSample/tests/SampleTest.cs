d1ccd736812cb88ba0dea565ac0bfe056a9961e8
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2019-08-12 01:56:32 -0700

    Refactor and add Comments

--- CSharpSeleniumWebdriverSample/CSharpSeleniumWebdriverSample/tests/SampleTest.cs
+++ CSharpSeleniumWebdriverSample/CSharpSeleniumWebdriverSample/tests/SampleTest.cs
@@ -17,7 +17,7 @@ using Selenium.Axe;
 namespace CSharpSeleniumWebdriverSample.Test
 {
     [TestClass]
-    [DeploymentItem("integration-test-target.html")]
+    [DeploymentItem("samplePage.html")]
     [DeploymentItem("chromedriver.exe")]
     [DeploymentItem("geckodriver.exe")]
     [TestCategory("Integration")]
@@ -25,10 +25,8 @@ namespace CSharpSeleniumWebdriverSample.Test
     {
         private IWebDriver _webDriver;
         private WebDriverWait _wait;
-        private static readonly string IntegrationTestTargetFile = Path.GetFullPath(@"src\samplePage.html");
-        private static readonly string IntegrationTestTargetUrl = new Uri(IntegrationTestTargetFile).AbsoluteUri;
-
-        private const string mainElementSelector = "main";
+        private const string MainElementSelector = "main";
+        private const int TimeOutInSeconds = 20;
 
         [TestCleanup]
         public virtual void TearDown()
@@ -43,28 +41,36 @@ namespace CSharpSeleniumWebdriverSample.Test
         [DataRow("Firefox")]
         public void RunScanOnGivenElement(string browser)
         {
-            this.InitDriver(browser);
-            LoadTestPage();
+            string samplePageURL = @"src\samplePage.html";
+            string integrationTestTargetFile = Path.GetFullPath(samplePageURL);
+            string integrationTestTargetUrl = new Uri(integrationTestTargetFile).AbsoluteUri;
+            string elementSelector = "ul";
+            int ExpectedNumberOfViolation = 3;
 
-            var mainElement = _wait.Until(drv => drv.FindElement(By.TagName("main")));
+            this.InitDriver(browser);
+            LoadTestPage(integrationTestTargetUrl);
+
+            var mainElement = _wait.Until(drv => drv.FindElement(By.TagName(elementSelector)));
 
             AxeResult results = _webDriver.Analyze(mainElement);
-            results.Violations.Should().HaveCount(3);
+            results.Violations.Should().HaveCount(ExpectedNumberOfViolation);
         }
 
 
-        private void LoadTestPage()
+        private void LoadTestPage(string integrationTestTargetUrl)
         {
-            _webDriver.Navigate().GoToUrl(IntegrationTestTargetUrl);
+            _webDriver.Navigate().GoToUrl(integrationTestTargetUrl);
 
-            _wait.Until(drv => drv.FindElement(By.TagName(mainElementSelector)));
+            _wait.Until(drv => drv.FindElement(By.TagName(MainElementSelector)));
         }
 
         private void InitDriver(string browser)
         {
             switch (browser.ToUpper())
             {
+                //In case using Chrome Web Driver
                 case "CHROME":
+                    // Check if Chrome web driver eniroment varibale already defined so that Chrome version and the webdriver are the same, other wise use Selenium.WebDriver.ChromeDriver to indtall it
                     var chromeDriverDirectory = Environment.GetEnvironmentVariable("ChromeWebDriver") ?? Environment.CurrentDirectory;
                     ChromeOptions options = new ChromeOptions
                     {
@@ -79,8 +85,9 @@ namespace CSharpSeleniumWebdriverSample.Test
                     _webDriver = new ChromeDriver(chromeDriverDirectory, options);
 
                     break;
-
+                // Incase Using Firefox
                 case "FIREFOX":
+                    // Check if Gecko web driver eniroment varibale already defined so that Firefox version and the webdriver are the same, other wise use Selenium.WebDriver.GeckoDriver to indtall it
                     var geckoDriverDirectory = Environment.GetEnvironmentVariable("GeckoWebDriver") ?? Environment.CurrentDirectory;
                     _webDriver = new FirefoxDriver(geckoDriverDirectory);
                     break;
@@ -90,8 +97,8 @@ namespace CSharpSeleniumWebdriverSample.Test
 
             }
 
-            _wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(20));
-            _webDriver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(20);
+            _wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(TimeOutInSeconds));
+            _webDriver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(TimeOutInSeconds);
             _webDriver.Manage().Window.Maximize();
         }
     }
