6a215a6a3f2984351882be0ec10fa01cc6528f2d
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2019-08-13 17:33:48 -0700

    change comment spaces indentation

--- csharp-selenium-webdriver-sample/SampleTest.cs
+++ csharp-selenium-webdriver-sample/SampleTest.cs
@@ -13,23 +13,23 @@ namespace CSharpSeleniumWebdriverSample.Test
    
     public class SampleTest
     {
-        //  Instance to control the browser.
-        //  The OpenQA.Selenium.IWebDriver interface is the main interface to use for testing, which represents an idealized web browser.
+        // Instance to control the browser.
+        // The OpenQA.Selenium.IWebDriver interface is the main interface to use for testing, which represents an idealized web browser.
         private IWebDriver _webDriver;
 
         [TestCleanup]
         public virtual void TearDown()
         {
-            //  You should quit the driver on TearDown which will close every associated window.
+            // You should quit the driver on TearDown which will close every associated window.
             _webDriver?.Quit();
         }
 
 
         [TestMethod]
-        //  First DataRow Attribute will create a new instance of the test method and pass Chrome as a paramter, so it'll test on ChromDriver
-        //  Second DataRow Attribute will create a new instance of the test method and pass Firefox as a paramter, so it'll test on FirefoxDriver
-        //  Instead of useing DataRow Attribute, you can create a helper fucntion that takes the the broswer type as a paremter
-        //  Then call it twice with 2 differrent test methods for each browser type
+        // First DataRow Attribute will create a new instance of the test method and pass Chrome as a paramter, so it'll test on ChromDriver
+        // Second DataRow Attribute will create a new instance of the test method and pass Firefox as a paramter, so it'll test on FirefoxDriver
+        // Instead of useing DataRow Attribute, you can create a helper fucntion that takes the the broswer type as a paremter
+        // Then call it twice with 2 differrent test methods for each browser type
         [DataRow(BrowserType.Chrome)]
         [DataRow(BrowserType.Firefox)]
         // This test case shows a basic example: run a scan on an element, return the result, make sure it's as expected.
@@ -39,17 +39,17 @@ namespace CSharpSeleniumWebdriverSample.Test
             this.InitDriver(browser);
             LoadTestPage();
 
-            //  Analyze the a known element in the page, and get the results
+            // Analyze the a known element in the page, and get the results
             AxeResult results = RunScanOnGivenElementBySelector("ul");
-            //  Assert that the number of violations that were found is that what you expect
+            // Assert that the number of violations that were found is that what you expect
             Assert.AreEqual(3, results.Violations.Length);
         }
 
         private AxeResult RunScanOnGivenElementBySelector(string elementSelector)
         {
-            //  Find the first element matching the element selector parameter
-            //  * It acts as a sanity check that our browser automation setup basically works
-            //  * It ensures that the page is loaded before we run our accessibility scans
+            // Find the first element matching the element selector parameter
+            // * It acts as a sanity check that our browser automation setup basically works
+            // * It ensures that the page is loaded before we run our accessibility scans
             var selectedElement = _webDriver.FindElement(By.TagName(elementSelector));
 
             //  Instruct the driver to analyze the current element, and return the result as AxeResult object
@@ -59,23 +59,23 @@ namespace CSharpSeleniumWebdriverSample.Test
 
         private void LoadTestPage()
         {
-            //  For simplicity, we're pointing our test browser directly to a static html file on disk.
-            //  In a project with more complex hosting needs, you might instead start up a localhost http server
-            //  and then navigate to a http://localhost link.
+            // For simplicity, we're pointing our test browser directly to a static html file on disk.
+            // In a project with more complex hosting needs, you might instead start up a localhost http server
+            // and then navigate to a http://localhost link.
             string integrationTestTargetFile = Path.GetFullPath(@"SamplePage.html");
             string integrationTestTargetUrl = new Uri(integrationTestTargetFile).AbsoluteUri;
 
-            //  Instructs the driver to navigate the browser to the target URL, this should start the brower and open the page in a new window tab
+            // Instructs the driver to navigate the browser to the target URL, this should start the brower and open the page in a new window tab
             _webDriver.Navigate().GoToUrl(integrationTestTargetUrl);
         }
 
         private void InitDriver(BrowserType browser)
         {
-            //  Create a web driver based on the bowser type which is ehither Chrome of Firefox
+            // Create a web driver based on the bowser type which is ehither Chrome of Firefox
             _webDriver = WebDriverFactory.GetWebDriver(browser);
-            //  Wait for 20 seconds when the driver is looking for an element eg: _webDriver.FindElement(By.TagName(elementSelector)); 
+            // Wait for 20 seconds when the driver is looking for an element eg: _webDriver.FindElement(By.TagName(elementSelector)); 
             _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-            //  Wait for 20 seconds when the driver is executing an asynchronism script
+            // Wait for 20 seconds when the driver is executing an asynchronism script
             _webDriver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(20);
         }
     }
--- csharp-selenium-webdriver-sample/WebDriverFactory.cs
+++ csharp-selenium-webdriver-sample/WebDriverFactory.cs
@@ -21,23 +21,23 @@ namespace CSharpSeleniumWebdriverSample
             switch (browser)
             {
                 case BrowserType.Chrome:
-                    //  So now we need to specify the ChromeDriver directory path
-                    //  We check first if the enivronemnt variable was set, if so we use the path value
-                    //  if it's not set, we use Selenium.WebDriver.ChromeDriver nuget to install it in the current directory
-                    //  chromedriver.exe does not appear in Solution Explorer, but it is copied to bin folder from package folder when the build process.
+                    // So now we need to specify the ChromeDriver directory path
+                    // We check first if the enivronemnt variable was set, if so we use the path value
+                    // if it's not set, we use Selenium.WebDriver.ChromeDriver nuget to install it in the current directory
+                    // chromedriver.exe does not appear in Solution Explorer, but it is copied to bin folder from package folder when the build process.
                     var chromeDriverDirectory = Environment.GetEnvironmentVariable("ChromeWebDriver") ?? Environment.CurrentDirectory;
 
-                    //  Initializes a new ChromeDriver using the specified path to the directory containing ChromeDriver.exe.
+                    // Initializes a new ChromeDriver using the specified path to the directory containing ChromeDriver.exe.
                     return new ChromeDriver(chromeDriverDirectory);
 
                 case BrowserType.Firefox:
-                    //  So now we need to specify the FirefoxDriver directory path
-                    //  We check first if the enivronemnt variable was set, if so we use the path value
-                    //  if it's not set, we use Selenium.WebDriver.GeckoDriver nuget to install it in the current directory
-                    //  geckodriver.exe does not appear in Solution Explorer, but it is copied to bin folder from package folder when the build process.           
+                    // So now we need to specify the FirefoxDriver directory path
+                    // We check first if the enivronemnt variable was set, if so we use the path value
+                    // if it's not set, we use Selenium.WebDriver.GeckoDriver nuget to install it in the current directory
+                    // geckodriver.exe does not appear in Solution Explorer, but it is copied to bin folder from package folder when the build process.           
                     var geckoDriverDirectory = Environment.GetEnvironmentVariable("GeckoWebDriver") ?? Environment.CurrentDirectory;
                     
-                    //  Initializes a new FirefoxDriver using the specified path to the directory containing GeckoDriver.exe
+                    // Initializes a new FirefoxDriver using the specified path to the directory containing GeckoDriver.exe
                     return new FirefoxDriver(geckoDriverDirectory);
 
                 default:
