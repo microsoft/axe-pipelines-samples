2831e0a7ce8c9bf836756234aea9fe053594fdba
AhmedAbdoOrtiga <45858632+AhmedAbdoOrtiga@users.noreply.github.com>
2019-08-14 15:42:29 -0700

    Add Inline Comments for the C# sample (#59)
    Add Inline Comments for the CSharpSeleniumWebdriverSample project.

--- csharp-selenium-webdriver-sample/SampleTest.cs
+++ csharp-selenium-webdriver-sample/SampleTest.cs
@@ -6,57 +6,73 @@ using OpenQA.Selenium;
 using System;
 using System.IO;
 using Selenium.Axe;
-using OpenQA.Selenium.Chrome;
 
 namespace CSharpSeleniumWebdriverSample.Test
 {
     [TestClass]
-    [DeploymentItem("SamplePage.html")]
-    [DeploymentItem("chromedriver.exe")]
-    [DeploymentItem("geckodriver.exe")]
     public class SampleTest
     {
+        // Instance to control the browser.
+        // The OpenQA.Selenium.IWebDriver interface is the main interface to use for testing, which represents an idealized web browser.
         private IWebDriver _webDriver;
 
         [TestCleanup]
         public virtual void TearDown()
         {
+            // You should quit the driver on TearDown which will close every associated window.
             _webDriver?.Quit();
         }
 
 
         [TestMethod]
+        // First DataRow Attribute will create a new instance of the test method and pass Chrome as a parameter, so it'll test on ChromeDriver
+        // Second DataRow Attribute will create a new instance of the test method and pass Firefox as a parameter, so it'll test on FirefoxDriver
+        // Instead of using DataRow Attribute, you can create a helper function that takes the browser type as a parameter
+        // Then call it twice with 2 different test methods for each browser type
         [DataRow(BrowserType.Chrome)]
         [DataRow(BrowserType.Firefox)]
+        // This test case shows a basic example: run a scan on an element, return the result, make sure it's as expected.
+        // This is the way to go if you have known/pre-existing violations you need to temporarily baseline.
         public void RunScanOnGivenElement(BrowserType browser)
         {
             this.InitDriver(browser);
             LoadTestPage();
 
-            AxeResult results = RunScanOnGivenElementBySelector("ul");
+            // Analyze the a known element in the page, and get the results
+            AxeResult results = RunScanOnGivenElementByTagName("ul");
+            // Assert that the number of violations that were found is that what you expect
             Assert.AreEqual(3, results.Violations.Length);
         }
 
-        private AxeResult RunScanOnGivenElementBySelector(string elementSelector)
+        private AxeResult RunScanOnGivenElementByTagName(string tagName)
         {
-            var selectedElement = _webDriver.FindElement(By.TagName(elementSelector));
+            // Find the first element matching the element tag name parameter
+            var selectedElement = _webDriver.FindElement(By.TagName(tagName));
 
+            //  Instruct the driver to analyze the current element, and return the result as AxeResult object
             return _webDriver.Analyze(selectedElement);
         }
 
 
         private void LoadTestPage()
         {
+            // For simplicity, we're pointing our test browser directly to a static html file on disk.
+            // In a project with more complex hosting needs, you might instead start up a localhost http server
+            // and then navigate to a http://localhost link.
             string integrationTestTargetFile = Path.GetFullPath(@"SamplePage.html");
             string integrationTestTargetUrl = new Uri(integrationTestTargetFile).AbsoluteUri;
 
+            // Instructs the driver to navigate the browser to the target URL, this should start the browser and open the page in a new window tab
             _webDriver.Navigate().GoToUrl(integrationTestTargetUrl);
         }
 
         private void InitDriver(BrowserType browser)
         {
+            // Create a web driver based on the bowser type which is either Chrome of Firefox
             _webDriver = WebDriverFactory.GetWebDriver(browser);
+            // Wait for 20 seconds when the driver is looking for an element eg: _webDriver.FindElement(By.TagName(elementSelector)); 
             _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            // Wait for 20 seconds when the driver is executing an asynchronism script
             _webDriver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(20);
         }
     }
--- csharp-selenium-webdriver-sample/WebDriverFactory.cs
+++ csharp-selenium-webdriver-sample/WebDriverFactory.cs
@@ -21,13 +21,29 @@ namespace CSharpSeleniumWebdriverSample
             switch (browser)
             {
                 case BrowserType.Chrome:
-                    // Check if Chrome web driver environment variable already defined so that Chrome version and the webdriver are the same, otherwise use Selenium.WebDriver.ChromeDriver to install it                    
+                    // The ChromeWebDriver environment variable comes pre-set in the Azure Pipelines VM Image we
+                    // specify in azure-pipelines.yml. ChromeDriver requires that you use *matching* versions of Chrome and
+                    // the Chrome WebDriver; in the build agent VMs, using this environment variable will make sure that we use
+                    // the pre-installed version of ChromeDriver that matches the pre-installed version of Chrome.
+                    //
+                    // See https://docs.microsoft.com/en-us/azure/devops/pipelines/test/continuous-test-selenium
+                    //
+                    // Environment.CurrentDirectory is where the Selenium.Webdriver.ChromeDriver NuGet package will place the
+                    // version of the Chrome WebDriver that it comes with. We fall back to this so that if a developer working on
+                    // the project hasn't separately installed ChromeDriver, the test will still be able to run on their machine.
                     var chromeDriverDirectory = Environment.GetEnvironmentVariable("ChromeWebDriver") ?? Environment.CurrentDirectory;
+
+                    // Initializes a new ChromeDriver using the specified path to the directory containing ChromeDriver.exe.
                     return new ChromeDriver(chromeDriverDirectory);
 
                 case BrowserType.Firefox:
-                    // Check if Gecko web driver environment variable already defined so that Firefox version and the webdriver are the same, otherwise use Selenium.WebDriver.GeckoDriver to install it                    
+                    // The same like above but for Firefox
+                    // the pre-set environment variable in the Azure Pipelines VM Image is GeckoWebDriver
+                    // the pre-installed version of FirefoxDriver matches the pre-installed version of Firefox
+                    // Selenium.WebDriver.GeckoDriver Nuget will place the Firefox WebDriver in the Environment.CurrentDirectory.
                     var geckoDriverDirectory = Environment.GetEnvironmentVariable("GeckoWebDriver") ?? Environment.CurrentDirectory;
+                    
+                    // Initializes a new FirefoxDriver using the specified path to the directory containing GeckoDriver.exe
                     return new FirefoxDriver(geckoDriverDirectory);
 
                 default:
